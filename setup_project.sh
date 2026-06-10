f6ae47596474b4e8c87ff3c7fb143db44d5fffb7
david-aurel-lenoir <d.butarebit@alustudent.com>
2026-06-10 15:37:21 -0700

    asks for the users name and check if they typed in the required infos

--- setup_project.sh
+++ setup_project.sh
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+ 
+# This makes all the folders and files for me
+ 
+# ask the user for the name
+echo "Welcome to my project setup!"
+echo "Please type a name for your projec:"
+read input
+ 
+# check that they typed something
+if [ "$input" = "" ]
+then
+	    echo "You did not type a name! Stopping."
+	        exit
+fi
